e9a640eae357784331d723d09c07513f9388684f
hasle1975 <38669079+Asatradingsoftware@users.noreply.github.com>
2021-04-14 19:18:18 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import {AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react';
+import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
+import Nav from './Nav';
+import MyHome from './MyHome';
+import About from './About';
+
+Amplify.configure(awsconfig)
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <header className="App-header">
+        
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/MyHome" component={MyHome} />
+          <Route path="/About" component={About} />
+          </Switch>
+      </header>
+    </div>
+    </Router>
+  );
+}
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+  </div>
+)
+
+export default withAuthenticator(App);
--- src/Nav.js
+++ src/Nav.js
@@ -10,10 +10,25 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import PieChartIcon from '@material-ui/icons/PieChart';
 import StarIcon from '@material-ui/icons/Star';
-import InsertChartIcon from '@material-ui/icons/InsertChart';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
 import { Link } from 'react-router-dom';
+import {AmplifySignOut} from '@aws-amplify/ui-react';
+import CardMedia from '@material-ui/core/CardMedia';
+import TranslateIcon from '@material-ui/icons/Translate';
+import LaptopMacIcon from '@material-ui/icons/LaptopMac';
+import MapIcon from '@material-ui/icons/Map';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import PublicIcon from '@material-ui/icons/Public';
+import LanguageIcon from '@material-ui/icons/Language';
+import PowerIcon from '@material-ui/icons/Power';
+import EuroIcon from '@material-ui/icons/Euro';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import MultilineChartIcon from '@material-ui/icons/MultilineChart';
+import TrendingDownIcon from '@material-ui/icons/TrendingDown';
+import TrendingFlatIcon from '@material-ui/icons/TrendingFlat';
+import Avatar from '@material-ui/core/Avatar';
+import LogoPic from './static/images/avatar/circleiconlogo.png'
 
 const drawerWidth = 240;
 
@@ -36,7 +51,7 @@ const useStyles = makeStyles((theme) => ({
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3),
+    padding: theme.spacing(1),
   },
 }));
 
@@ -48,6 +63,12 @@ export default function ClippedDrawer() {
       <CssBaseline />
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
+        <CardMedia
+              className={classes.media}
+              image="./static/images/avatar/logo_rion.png"
+              title="Paella dish"
+          />
+          <Avatar alt="Remy Sharp" src={LogoPic} style={{ marginRight: '0.8rem' }} />
           <Typography variant="h6" noWrap>
             OrionZolution ver 0.1
           </Typography>
@@ -63,43 +84,45 @@ export default function ClippedDrawer() {
         <Toolbar />
         <div className={classes.drawerContainer}>
           <List>
-            
+            <Typography variant="h6" gutterBottom>
+                Investment Categories
+              </Typography>              
               <ListItem button key="USequities">
                 <Link to="/MyHome" className="nav-links">
-                <ListItemIcon><InsertChartIcon /></ListItemIcon>
+                <ListItemIcon><AttachMoneyIcon /></ListItemIcon>
                 <ListItemText primary="USequities" />
                 </Link>
               </ListItem>
               <ListItem button key="Technology">
-                <ListItemIcon><InsertChartIcon /></ListItemIcon>
+                <ListItemIcon><LaptopMacIcon /></ListItemIcon>
                 <ListItemText primary="Technology" />
               </ListItem>     
-              <ListItem button key="MIddleeast">
-                <ListItemIcon><InsertChartIcon /></ListItemIcon>
-                <ListItemText primary="MIddleeast" />
+              <ListItem button key="Middleeast">
+                <ListItemIcon><MapIcon /></ListItemIcon>
+                <ListItemText primary="Middleeast" />
               </ListItem>  
               <ListItem button key="Healthcare">
-                <ListItemIcon><InsertChartIcon /></ListItemIcon>
+                <ListItemIcon><LocalHospitalIcon /></ListItemIcon>
                 <ListItemText primary="Healthcare" />
               </ListItem>  
               <ListItem button key="Global">
-                <ListItemIcon><InsertChartIcon /></ListItemIcon>
+                <ListItemIcon><PublicIcon /></ListItemIcon>
                 <ListItemText primary="Global" />
               </ListItem>  
               <ListItem button key="Emerging Markets">
-                <ListItemIcon><InsertChartIcon /></ListItemIcon>
+                <ListItemIcon><LanguageIcon /></ListItemIcon>
                 <ListItemText primary="Emerging Markets" />
               </ListItem>  
               <ListItem button key="China">
-                <ListItemIcon><InsertChartIcon /></ListItemIcon>
+                <ListItemIcon><TranslateIcon /></ListItemIcon>
                 <ListItemText primary="China" />
               </ListItem>  
               <ListItem button key="Energy">
-                <ListItemIcon><InsertChartIcon /></ListItemIcon>
+                <ListItemIcon><PowerIcon /></ListItemIcon>
                 <ListItemText primary="Energy" />
               </ListItem>                                                                        
               <ListItem button key="Europe">
-                <ListItemIcon><InsertChartIcon /></ListItemIcon>
+                <ListItemIcon><EuroIcon /></ListItemIcon>
                 <ListItemText primary="Europe" />
               </ListItem>    
             
@@ -114,25 +137,26 @@ export default function ClippedDrawer() {
           <Divider />
           <List>
                 <ListItem button key="Disflation">
-                <ListItemIcon><PieChartIcon /></ListItemIcon>
+                <ListItemIcon><TrendingFlatIcon /></ListItemIcon>
                 <ListItemText primary="Disflation" />
                 </ListItem>    
 
                 <ListItem button key="Deflation">
-                <ListItemIcon><PieChartIcon /></ListItemIcon>
+                <ListItemIcon><TrendingDownIcon /></ListItemIcon>
                 <ListItemText primary="Deflation" />
                 </ListItem>    
 
                 <ListItem button key="Inflation">
-                <ListItemIcon><PieChartIcon /></ListItemIcon>
+                <ListItemIcon><TrendingUpIcon /></ListItemIcon>
                 <ListItemText primary="Inflation" />
                 </ListItem>    
 
                 <ListItem button key="Stagflation">
-                <ListItemIcon><PieChartIcon /></ListItemIcon>
+                <ListItemIcon><MultilineChartIcon /></ListItemIcon>
                 <ListItemText primary="Stagflation" />
                 </ListItem>    
-          </List>                                        
+          </List>     
+          <AmplifySignOut />                                   
         </div>
       </Drawer>
       <main className={classes.content}>
